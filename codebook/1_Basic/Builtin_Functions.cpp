efcda50890a1fda30fa6fd5a569286b874436085
JoyLiao0211 <liaojoy985@gmail.com>
2025-09-14 01:42:11 +0800

    useful builtin funs

---
+++ codebook/1_Basic/Builtin_Functions.cpp
@@ -0,0 +1,5 @@
+int __builtin_popcount(int a); // count number of 1s
+int __lg(int a); // floor(log_2(a))
+int __gcd(int a,int b); // gcd
+iota(a.begin(), a.end(), 1); // fill a, start from 1
+bool next_permutation(all(a)); // return 0 if end
