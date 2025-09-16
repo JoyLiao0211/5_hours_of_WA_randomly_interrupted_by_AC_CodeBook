e1995e7c55d05c05aecd725f2c0b29be032eea79
illumeow <illumeow0223@gmail.com>
2025-09-16 13:59:29 +0800

    set cursorline in vimrc

--- codebook/1_Basic/vimrc.cpp
+++ codebook/1_Basic/vimrc.cpp
@@ -3,4 +3,6 @@ se re=1 ts=4 sts=4 sw=4 ls=2 mouse=a
 syntax on
 set bg=dark
 ino {<CR> {<CR>}<Esc>ko<tab>
+hi cursorline cterm=none ctermbg=0
+hi cursorlinenr cterm=bold
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
