ce5d8563d01ff61ead4e72567d54670c15988ebd
Hyper <hypersoweak@gmail.com>
2025-09-19 17:24:10 +0800

    推上去

--- codebook/11_Python/misc.py
+++ codebook/11_Python/misc.py
@@ -3,3 +3,6 @@ setcontext(Context(prec=MAX_PREC, Emax=MAX_EMAX, rounding=ROUND_FLOOR))
 print(Decimal(input()) * Decimal(input()))
 from fractions import Fraction
 Fraction('3.14159').limit_denominator(10).numerator # 22
+sys.set_int_max_str_digits(int(1e6))
+a, b = map(int, input().split())
+a = map(int, input().split())  # array
