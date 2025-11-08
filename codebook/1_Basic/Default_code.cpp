cbf80dfafe035d22e1d13edca190f25aaeee067b
JoyLiao0211 <liaojoy985@gmail.com>
2025-11-08 15:25:51 +0800

    add def int ll, vp, vvp

--- codebook/1_Basic/Default_code.cpp
+++ codebook/1_Basic/Default_code.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define int long long
 #define fi first
 #define se second
 #define all(a) a.begin(),a.end()
@@ -12,19 +13,8 @@ using ll = long long;
 using pii = pair<int,int>;
 using vi = vector<int>;
 using vvi = vector<vi>;
-template<typename T, typename U>ostream& operator<<(ostream&os,const pair<T,U>&p)
-{return os<<"("<<p.first<<", "<<p.second<<")";}
-template<typename T> ostream& operator<<(ostream&os,const vector<T>&v)
-{os<<"[";for(int i=0;i<v.size();++i)os<<v[i]<<(i==v.size()-1?"]":", ");return os;}
-#ifdef D
-template<typename T> void _dbg(T x){cerr<<x<<"\n";}
-template<typename T,typename ...U> void _dbg(T x,U ...y){cerr<<x<<", ";_dbg(y...);}
-#define dbg(...) do{cerr<<"\033[1;33m"<<#__VA_ARGS__<<" = ";_dbg(__VA_ARGS__);cerr<<"\033[0m";}while(0)
-#else
-#define dbg(...)
-#endif
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3f;
+using vp=vector<pii>;
+using vvp=vector<vp>;
 
 void foo(){
     
